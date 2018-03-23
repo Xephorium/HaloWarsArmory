e5527ed305e59eefd13072b4592c5621774312f7
unknown <christopher.a.cruzen@gmail.com>
2018-03-23 01:46:21 -0500

    Add Rough Install Directory Bar

--- src/com/xephorium/armory/java/ui/ArmoryWindow.java
+++ src/com/xephorium/armory/java/ui/ArmoryWindow.java
@@ -1,6 +1,8 @@
 package com.xephorium.armory.java.ui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class ArmoryWindow {
@@ -8,8 +10,8 @@ public class ArmoryWindow {
 
     /*--- Variables ---*/
 
-    private final int WINDOW_HEIGHT = 550;
-    private final int WINDOW_WIDTH = 800;
+    private final int WINDOW_HEIGHT = 500;
+    private final int WINDOW_WIDTH = 700;
 
     private final String WINDOW_TITLE = "  Armory";
 
@@ -42,5 +44,28 @@ public class ArmoryWindow {
         JPanel panel = new JPanel();
         panel.setBackground(new Color(144, 198, 252));
         frame.add(panel, BorderLayout.CENTER);
+
+        JPanel installDirectoryPanel = new JPanel();
+        installDirectoryPanel.setLayout(new BoxLayout(installDirectoryPanel, BoxLayout.X_AXIS));
+        installDirectoryPanel.setBorder(new EmptyBorder(20, 20, 10, 20));
+        installDirectoryPanel.setBackground(new Color(255, 255, 255));
+
+
+        ImageIcon imageIcon = new ImageIcon("src\\com\\xephorium\\armory\\res\\icon\\iconCheckmark.png", "Checkmark Icon");
+        JLabel installIconLabel = new JLabel("", imageIcon, JLabel.CENTER);
+
+        JTextField installDirectoryField = new JTextField();
+        installDirectoryField.setText("Choose an install directory...");
+        installDirectoryField.setEnabled(false);
+
+        JButton browseButton = new JButton("Browse");
+
+        installDirectoryPanel.add(installIconLabel);
+        installDirectoryPanel.add(new Box.Filler(new Dimension(10, 0), new Dimension(10, 0), new Dimension(10, 0)));
+        installDirectoryPanel.add(installDirectoryField);
+        installDirectoryPanel.add(new Box.Filler(new Dimension(10, 0), new Dimension(10, 0), new Dimension(10, 0)));
+        installDirectoryPanel.add(browseButton);
+
+        frame.add(installDirectoryPanel, BorderLayout.PAGE_START);
     }
 }
