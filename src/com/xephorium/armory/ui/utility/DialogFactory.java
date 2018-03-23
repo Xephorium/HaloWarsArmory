1f592ae5aac0019b9e527e99a2670225c91c2dd6
unknown <christopher.a.cruzen@gmail.com>
2018-03-23 17:05:57 -0500

    Update Dialog Icons

--- src/com/xephorium/armory/ui/utility/DialogFactory.java
+++ src/com/xephorium/armory/ui/utility/DialogFactory.java
@@ -1,14 +1,24 @@
 package com.xephorium.armory.ui.utility;
 
+import com.xephorium.armory.ui.resource.image.ArmoryImage;
+
 import javax.swing.*;
 
 public class DialogFactory {
 
     public static void createGameNotFoundDialog(JFrame frame) {
-        JOptionPane.showMessageDialog(frame, "No Halo Wars installation found.", "Game Not Found", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(frame,
+                "No Halo Wars installation found.",
+                "Game Not Found",
+                JOptionPane.ERROR_MESSAGE,
+                ArmoryImage.ICON_INSTALLATION_INVALID);
     }
 
     public static void createGameFoundDialog(JFrame frame) {
-        JOptionPane.showMessageDialog(frame, "Installation found!", "Game Found", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(frame,
+                "Installation found!",
+                "Game Found",
+                JOptionPane.INFORMATION_MESSAGE,
+                ArmoryImage.ICON_INSTALLATION_FOUND);
     }
 }
